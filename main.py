b3335c817584c50b81392b140943c28e0ff01220
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2022-06-28 23:47:03 +0530

    gmail authentication & basic text and html mail senting finished

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello World")
+from src.google.gmail import GmailBase
+
+
+CLIENT_FILE = "credentials.json"
+client = GmailBase(CLIENT_FILE)
+
+to = 'adnankattekaden2020@gmail.com'
+subject = 'Congratulations!! You had successfully completed the bootcamp Build.myweb v2'
+body = """
+<p>Simple HTML.</p>
+<h3><em>So very simple.</em></h3>
+<p><span style="color: #ff0000;">Lame joke that follows.</span></p>
+<p>
+  <span style="color: #ff0000;"
+    ><img
+      src="https://i.imgur.com/mxVlFHM.jpeg"
+      alt="Lame joke"
+      width="245"
+      height="221"
+  /></span>
+</p>
+<h2 style="padding-left: 30px;">do you?</h2>
+<ul>
+  <li>yes</li>
+  <li>no</li>
+  <li><strong>not entirely sure</strong></li>
+</ul>
+"""
+
+client.create_message(to,subject,body)
---
+++ src/google/gmail.py
@@ -0,0 +1,47 @@
+from src.google.google_authentication import Create_Service
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+class GmailBase:
+    API_NAME = 'gmail'
+    API_VERSION = 'v1'
+    SCOPES = ['https://mail.google.com/']
+    
+    def __init__(self,CLIENT_FILE) -> None:
+        """
+        Create an instance and Authenticate User and 
+        Assign self.service Authorized Gmail API 
+        service instance.
+        
+        Args:
+            CLIENT_FILE: Path of Authorized Gmail API Data
+        """
+
+        self.service = Create_Service(CLIENT_FILE,self.API_NAME,self.API_VERSION,self.SCOPES)
+        self.service.users().getProfile(userId='me').execute()
+        
+    def create_message(self,to,subject,body,message_type='plain',user_id='me'):
+        """
+        Send an email message.
+        
+        Args:
+            to: Email address of the receiver
+            subject: The subject of the email message
+            body: The Body (content) of email
+            message_type: HTML / Plain Text 
+            user_id: User's email address. The special value "me"
+            can be used to indicate the authenticated user.
+        
+        Returns:
+            Message.
+        """
+        
+        mimeMessage = MIMEMultipart()
+        mimeMessage['to'] = to
+        mimeMessage['subject'] = subject
+        mimeMessage.attach(MIMEText(body,message_type))
+
+        raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+        message = self.service.users().messages().send(userId=user_id,body={'raw':raw_string}).execute()
+        return message
